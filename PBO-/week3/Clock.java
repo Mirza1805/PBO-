0fecf44641051d4baf47005a6acc7a91d1c75a12
Muhammad Mirza Rafi'uddin <112935970+Mirza1805@users.noreply.github.com>
2024-05-02 23:06:18 +0700

    Delete PBO- directory

--- PBO-/week3/Clock.java
+++
@@ -1,9 +0,0 @@
-package week3;
-public class Clock{
-    public static void main(String[]args){
-        ClockDisplay jam = new ClockDisplay(14,30);
-        
-        jam.timeTick();
-        System.out.println(jam.getTime());
-    }
-}
--- PBO-/week7/DataMahasiswaAktif.java
+++
@@ -1,8 +0,0 @@
-package week7;
-public class DataMahasiswaAktif{
-    public static void main(String[]args){
-        MahasiswaAktif mhs1=new MahasiswaAktif("Mirza","A11","25 Mei",5);
-        mhs1.getAktif();
-
-    }
-}
